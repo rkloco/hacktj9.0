6ac1185325f04c3bd29971b6e6d5919652baf63f
rkloco <81713063+rkloco@users.noreply.github.com>
2022-04-17 01:28:40 -0400

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import pic from './plots/scatter0.png';
+
+function App(props) {
+  const [getMessage, setGetMessage] = useState({})
+
+  useEffect(()=>{
+    axios.get('http://localhost:5000/flask/hello?name='+props.name).then(response => {
+      console.log("SUCCESS", response)
+      setGetMessage(response)
+    }).catch(error => {
+      console.log(error)
+    })
+
+  }, [])
+  var mynum = '4'
+//<img src={`data:image/png};base64,${getMessage.data.img}`}></img>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Coordinates:</p>
+        <div>{getMessage.status === 200 ? 
+          <h3>{getMessage.data.message}</h3>
+          :
+          <h3>LOADING</h3>}
+        </div>
+        
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
